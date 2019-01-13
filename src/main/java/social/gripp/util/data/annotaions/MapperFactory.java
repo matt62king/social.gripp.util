233526abc3a7a6617aa4bdc2cdde7a7a9ecf0149
matt62king <matt62king@gmail.com>
2019-01-12 18:26:25 -0600

    added custom annotaions

---
+++ src/main/java/social/gripp/util/data/annotaions/MapperFactory.java
@@ -0,0 +1,17 @@
+package social.gripp.util.data.annotaions;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import social.gripp.util.QualifierNames;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Qualifier(QualifierNames.MAPPER)
+public @interface MapperFactory {
+}
