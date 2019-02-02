857ccff856d54d14773e5b0faaa0e34951588cf8
matt62king <matt62king@gmail.com>
2019-02-02 16:24:06 -0600

    added cache name static values

---
+++ src/main/java/social/gripp/util/data/cache/TaskCacheNames.java
@@ -0,0 +1,7 @@
+package social.gripp.util.data.cache;
+
+public interface TaskCacheNames {
+    String TASK = "task";
+    String TASK_LIST = "task";
+    String SUBTASK_LINKER_LIST = "subTaskLinkerList";
+}
