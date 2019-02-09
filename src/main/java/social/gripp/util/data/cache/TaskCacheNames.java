952d8cb0a47eda1e961fd3301284eefe23c1caad
matt62king <matt62king@gmail.com>
2019-02-08 22:10:15 -0600

    added task branch cache names

--- src/main/java/social/gripp/util/data/cache/TaskCacheNames.java
+++ src/main/java/social/gripp/util/data/cache/TaskCacheNames.java
@@ -3,5 +3,7 @@ package social.gripp.util.data.cache;
 public interface TaskCacheNames {
     String TASK = "task";
     String TASK_LIST = "taskList";
+    String TASK_BRANCH = "taskBranch";
+    String TASK_BRANCH_LIST = "taskBranchList";
     String SUBTASK_LINKER_LIST = "subTaskLinkerList";
 }
