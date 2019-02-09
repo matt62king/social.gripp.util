09f22309fccaa142fae56a595e3c792afef158bb
matt62king <matt62king@gmail.com>
2019-02-09 13:42:08 -0600

    added cache keys

--- src/main/java/social/gripp/util/data/cache/ProjectCacheNames.java
+++ src/main/java/social/gripp/util/data/cache/ProjectCacheNames.java
@@ -4,4 +4,6 @@ public interface ProjectCacheNames {
     String PROJECT_REPO = "projectRepo";
     String PROJECT_REPO_LIST = "projectRepoList";
     String PROJECT_WORKFLOW = "projectWorkflow";
+    String PROJECT_WORKFLOW_TRANSITION = "projectWorkflowTransition";
+    String PROJECT_WORKFLOW_TRANSITION_LIST = "projectWorkflowTransitionList";
 }
