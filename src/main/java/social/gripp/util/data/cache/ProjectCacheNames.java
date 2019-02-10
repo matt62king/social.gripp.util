c46f0dc09b45e0c69fe4853df4116ccf0aeb9d51
matt62king <matt62king@gmail.com>
2019-02-10 15:47:42 -0600

    added label cache names

--- src/main/java/social/gripp/util/data/cache/ProjectCacheNames.java
+++ src/main/java/social/gripp/util/data/cache/ProjectCacheNames.java
@@ -1,6 +1,8 @@
 package social.gripp.util.data.cache;
 
 public interface ProjectCacheNames {
+    String PROJECT_LABEL = "projectLabel";
+    String PROJECT_LABEL_LIST = "projectLabelList";
     String PROJECT_REPO = "projectRepo";
     String PROJECT_REPO_LIST = "projectRepoList";
     String PROJECT_WORKFLOW = "projectWorkflow";
