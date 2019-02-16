57cd61b3b70447698e3362a3c85f2135bd6555c2
matt62king <matt62king@gmail.com>
2019-02-15 23:16:22 -0600

    added mapper configuration

---
+++ src/main/java/social/gripp/util/data/mapper/configuration/DelegatingMapperConfiguration.java
@@ -0,0 +1,42 @@
+package social.gripp.util.data.mapper.configuration;
+
+import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.impl.DefaultMapperFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import social.gripp.util.QualifierNames;
+import social.gripp.util.data.mapper.converter.UUIDConverter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class DelegatingMapperConfiguration {
+
+    private final UUIDConverter uuidConverter = new UUIDConverter();
+
+    private List<MapperConfiguration> mapperConfigurations = new ArrayList<>();
+
+    @Autowired(required = false)
+    public void setConfigurers(List<MapperConfiguration> mapperConfigurations) {
+        this.mapperConfigurations = mapperConfigurations;
+    }
+
+    @Bean
+    @Qualifier(QualifierNames.MAPPER)
+    public MapperFactory createMapperFactory() {
+        MapperFactory mapperFactory = new DefaultMapperFactory.Builder().build();
+        mapperFactory.getConverterFactory().registerConverter(uuidConverter);
+
+        mapperConfigurations.forEach(mapperConfiguration -> {
+            mapperConfiguration.registerConverters(mapperFactory);
+            mapperConfiguration.registerMappers(mapperFactory);
+        });
+//        registerConverters(mapperFactory);
+//        registerMappers(mapperFactory);
+
+        return mapperFactory;
+    }
+}
---
+++ src/main/java/social/gripp/util/data/mapper/configuration/EnableGrippMapper.java
@@ -0,0 +1,16 @@
+package social.gripp.util.data.mapper.configuration;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+@Documented
+@Import({DelegatingMapperConfiguration.class})
+public @interface EnableGrippMapper {
+}
