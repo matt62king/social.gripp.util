0c4c23afc46d41e64e19460bd950fd091f1a62f1
matt62king <matt62king@gmail.com>
2019-02-16 19:32:51 -0600

    added project cache names

--- src/main/java/social/gripp/util/data/cache/ProjectCacheNames.java
+++ src/main/java/social/gripp/util/data/cache/ProjectCacheNames.java
@@ -4,6 +4,8 @@ public interface ProjectCacheNames {
     String PROJECT = "project";
     String PROJECT_BOARD = "projectBoard";
     String PROJECT_BOARD_LIST = "projectBoardList";
+    String PROJECT_BOARD_LINKER = "projectBoardLinker";
+    String PROJECT_BOARD_LINKER_LIST = "projectBoardLinkerList";
     String PROJECT_BRANCH = "projectBranch";
     String PROJECT_BRANCH_LIST = "projectBranchList";
     String PROJECT_LABEL = "projectLabel";
@@ -16,7 +18,13 @@ public interface ProjectCacheNames {
     String PROJECT_REPO_BRANCH_USER_PERMISSON_LIST = "projectRepoBranchUserPermissionList";
     String PROJECT_SPRINT = "projectSprint";
     String PROJECT_SPRINT_LIST = "projectSprintList";
+    String PROJECT_USER = "projectUser";
+    String PROJECT_USER_LIST = "projectUserList";
+    String PROJECT_USER_LINKER = "projectUserLinker";
+    String PROJECT_USER_LINKER_LIST = "projectUserLinkerList";
     String PROJECT_WORKFLOW = "projectWorkflow";
+    String PROJECT_WORKFLOW_LINKER = "projectWorkflowLinker";
+    String PROJECT_WORKFLOW_LINKER_LIST = "projectWorkflowLinkerList";
     String PROJECT_WORKFLOW_TRANSITION = "projectWorkflowTransition";
     String PROJECT_WORKFLOW_TRANSITION_LIST = "projectWorkflowTransitionList";
 }
