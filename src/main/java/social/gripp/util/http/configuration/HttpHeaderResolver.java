d4c749d970c7d38485472891d63b97be280f1ec4
matt62king <matt62king@gmail.com>
2019-03-23 00:34:14 -0500

    UTIL-8 addded http configuration

--- src/main/java/social/gripp/util/http/configuration/HttpHeaderResolver.java
+++ src/main/java/social/gripp/util/http/configuration/HttpHeaderResolver.java
@@ -1,13 +1,11 @@
 package social.gripp.util.http.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import social.gripp.util.http.GrippHttpHeaders;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
-@Component
 public class HttpHeaderResolver {
 
     @Autowired
@@ -15,5 +13,9 @@ public class HttpHeaderResolver {
 
     public Optional<String> getProjectIdHeader() {
         return Optional.ofNullable(httpServletRequest.getHeader(GrippHttpHeaders.PROJECT_ID_HEADER));
+    }
+
+    public boolean hasHeader(String header) {
+        return httpServletRequest.getHeader(header) != null && !httpServletRequest.getHeader(header).isEmpty();
     }
 }
---
+++ src/main/java/social/gripp/util/http/validation/HeaderValidatorAspect.java
@@ -0,0 +1,25 @@
+package social.gripp.util.http.validation;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import social.gripp.util.http.configuration.HttpHeaderResolver;
+
+@Aspect
+@Component
+public class HeaderValidatorAspect {
+
+    @Autowired(required = false)
+    private HttpHeaderResolver httpHeaderResolver;
+
+    @Before("@annotation(ValidateHeader)")
+    public Object validateHeader(ProceedingJoinPoint proceedingJoinPoint, ValidateHeader validateHeader) throws Throwable {
+        if (httpHeaderResolver != null && httpHeaderResolver.hasHeader(validateHeader.value())) {
+            return proceedingJoinPoint.proceed();
+        } else {
+            throw new Exception(validateHeader.value() + " is a required header");
+        }
+    }
+}
